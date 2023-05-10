cd4e1e9b5a96fec9c846a0cfc1bd91f5a1dd98ce
qwe82950223 <linxiao0106@gmail.com>
2023-05-10 00:59:24 -0400

    update

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,10 +1,12 @@
 
 
 export default async function handler(req, res) {
-    const results = await fetch('https://252793659525377:xySFKsD1JbyXUKV8AoPKXBk3yw8@api.cloudinary.com/v1_1/xiaocloud/resources/image')
-    .then(r => r);
-    console.log("1")
-    console.log(result)
+  const response = await fetch(`https://api.cloudinary.com/v1_1/xiaocloud/resources/image`, {
+    headers: {
+      Authorization: `Basic ${Buffer.from('252793659525377:xySFKsD1JbyXUKV8AoPKXBk3yw8').toString('base64')}`
+    }
+  })
+  const images = await response.json()
 
-  res.status(200).json({ ...results });
+  res.status(200).json(images)
 }
