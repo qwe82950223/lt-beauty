503112f168baae75a61d0ff1334d126542639a42
qwe82950223 <linxiao0106@gmail.com>
2025-09-23 21:32:47 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,23 @@ export default function Document() {
   return (
     <Html>
       <Head> 
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer1 = window.dataLayer1 || [];
+            `,
+          }}
+        />
+         {/* Google Tag Manager - Container 1 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer1','AW-17574446995');`,
+          }}
+        />
         <meta property="og:title" content="LT Beauty Great Neck, New York" />
         <link rel="icon" href="/images/logo.png" />
       </Head>
